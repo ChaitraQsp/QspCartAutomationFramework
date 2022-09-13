dd53af5b9fd04bb9fc105427d5430d8271acdbda
Chaitra9632 <chaitra.sreenivas88@gmail.com>
2022-09-13 10:19:16 +0530

    First commit by engineer 1 - addToCartTest

---
+++ src/test/java/practice/AddToCartTest.java
@@ -0,0 +1,13 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class AddToCartTest {
+	
+	@Test
+	public void addToCartTest()
+	{
+		System.out.println("Product Added");
+	}
+
+}
